92eb4e6c50fd0c6e0cc9f603ea12b318f47e9a16
MarianeSilva256 <marysilva5667@gmail.com>
2026-05-08 18:00:40 -0300

    tela de login pronta

---
+++ vite-react-lab/src/App.jsx
@@ -0,0 +1,11 @@
+import Login from "./componentes/login/Login"
+
+
+function App() {
+  return (
+    <Login/>
+    
+  )
+}
+
+export default App
---
+++ vite-react-lab/src/componentes/login/Login.jsx
@@ -0,0 +1,19 @@
+function Login() {
+    return (
+        <div>
+            <h2>login</h2>
+            <form>
+                <div>
+                    <input type="email"placeholder="Digite seu email"></input>
+                </div>
+                <br/>
+                <div>
+                    <input type="password" placeholder="Digite sua senha"></input>
+                    </div>
+                    <br/>
+                    <button type="submit">Entrar</button>
+            </form>
+        </div>
+    )
+}
+export default Login
